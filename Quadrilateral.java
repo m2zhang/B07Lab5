e0aeaa0aa094a9be1692ea2d9235cc1e7e717557
avincenttjhin <avtjhin@gmail.com>
2024-06-24 15:16:30 -0400

    Updated Quadrilateral.java

--- Quadrilateral.java
+++ Quadrilateral.java
@@ -1,10 +1,10 @@
-public class Quadrilateral {
+public class Sqaure {
 	Point A;
 	Point B;
 	Point C;
 	Point D;
 	
-	public Quadrilateral(Point A, Point B, Point C, Point D) {
+	public Square(Point A, Point B, Point C, Point D) {
 		this.A = A;
 		this.B = B;
 		this.C = C;
@@ -15,7 +15,7 @@ public class Quadrilateral {
 		return A.distance(B) + B.distance(C) + C.distance(D) + D.distance(A);
 	}
 	
-	public boolean isSqaure() {
+	public boolean isValidSqaure() {
 		double dAB = A.distance(B);
 		double dBC = B.distance(C);
 		double dCD = C.distance(D);
@@ -26,14 +26,4 @@ public class Quadrilateral {
 			return false;
 	}
 
-	public boolean isRectangle() {
-		double dAB = A.distance(B);
-		double dBC = B.distance(C);
-		double dCD = C.distance(D);
-		double dDA = D.distance(A);
-		if(dAB==dCD && dBC==dDA)
-			return true;
-		else
-			return false;
-	}
 }
