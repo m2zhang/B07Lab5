2f79130ec08ddd5b41cff57ad1f396a7c7dfbc38
ashyng <ashley.yanyun.yang@gmail.com>
2024-06-28 15:43:51 -0400

    test:add test cases for Circle class

--- testcircle.java
+++ testcircle.java
@@ -7,9 +7,15 @@ class testcircle {
 	void testPerimeter() {
 		Point p1 = new Point(0, 0);
 		Circle c1 = new Circle(3.0, p1);
+		Circle c2 = new Circle(0.0, p1);
+		Circle c3 = new Circle(2.8, p1);
 		
 		double x1 = c1.perimeter();
 		assertEquals(x1, 6.0 * Math.PI);
+		double x2 = c2.perimeter();
+		assertEquals(x2, 0.0);
+		double x3 = c3.perimeter();
+		assertEquals(x3, 5.6 * Math.PI);
 		
 	}
 }
