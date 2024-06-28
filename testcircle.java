7cfc9919125d9fd0537039574169e3d3acf90ee1
ashyng <ashley.yanyun.yang@gmail.com>
2024-06-27 23:12:58 -0400

    test:add test cases for Circle class

---
+++ testcircle.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testcircle {
+
+	@Test
+	void testPerimeter() {
+		Point p1 = new Point(0, 0);
+		Circle c1 = new Circle(3.0, p1);
+		
+		double x1 = c1.perimeter();
+		assertEquals(x1, 6.0 * Math.PI);
+		
+	}
+}
